3384d9b69ba084779f4ae9ae086f77c7ef1a8058
Jerel R Hass <agies1@hotmail.com>
2014-05-23 00:07:42 -0400

    allow for injected request validator

--- WCF.Validation.Demo.Web/SomeOtherValidator.cs
+++ WCF.Validation.Demo.Web/SomeOtherValidator.cs
@@ -1,7 +1,13 @@
-﻿namespace WCF.Validation.Demo.Web
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WCF.Validation.Demo.Web
 {
     public class SomeOtherValidator : AnnotationAndValidatableRequestValidator
     {
-         
+        protected override void OnValidate(object input, ValidationContext context)
+        {
+            base.OnValidate(input, context);
+            ModelState.Current.AddModelError("TheOtherOne", "I'm hidden");
+        }
     }
 }
